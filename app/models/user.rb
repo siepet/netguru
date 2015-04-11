7c6eae1a3d9889c0d964c2b1cf8309b3fc913287
siepet <siepet7@gmail.com>
2015-04-11 17:27:17 +0100

    added validations for firstname and lastname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+	validates_presence_of :firstname
+	validates_presence_of :lastname
   has_many :reviews
   has_many :products
+
 end
