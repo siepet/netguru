c66fcbbb25e609c6726b65bcf65a9534aa69f6bf
siepet <siepet7@gmail.com>
2015-04-12 19:27:46 +0100

    fixed tabs to spaces

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+	
+  validates_presence_of :content
+  validates_presence_of :rating
+  validates_presence_of :user_id
+
 end
