ba2740ad2eecceac88a1ea3b24d2acbe09ed0062
siepet <siepet7@gmail.com>
2015-04-12 17:30:51 +0100

    added method to calculate average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,12 @@ class Product < ActiveRecord::Base
 
   validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
 	
-
+  def average_rating
+	  rating = 0.0
+    self.reviews.each do |r|
+		  rating += r.rating
+		end
+		rating / self.reviews.count
+	end
 end
 
