6f77343fd30d2cca937a57e9b06369d106f161b4
siepet <siepet7@gmail.com>
2015-04-12 17:25:56 +0100

    added validations for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+	validates_presence_of :title
+	validates_presence_of :description
+	validates_presence_of :price
+
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+	
+
 end
+
