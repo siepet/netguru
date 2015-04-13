a4495f0f86033f88e935a50f3fc355d8b403ce87
siepet <siepet7@gmail.com>
2015-04-13 20:17:26 +0100

    added scope for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
       resources :reviews
     end
   end
+  
+  scope '/profile' do
+    match ':id', to: 'users#show', as: "profile", via: 'get'
+  end
+	
 	devise_for :users
   root 'categories#index'
 end
